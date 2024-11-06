02afae0dca36547a13c0bda1777be1867f1219d1
Felipe <felipe.silva@acct.global>
2024-11-06 16:14:34 -0300

    getproducts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,53 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000";
 
-function main() {
-  console.log(serverUrl);
+async function main() {
+  console.log(serverUrl)
+  console.log(fetchProducts());
+  const productsList =fetchProducts()
+  listProducts(await productsList);
+}
+async function fetchProducts(): Promise<Product[]> {
+
+  try {
+    const reponse = await fetch(`${serverUrl}/products`)
+    const products = await reponse.json()
+    return products
+  }
+
+  catch (error) {
+    console.error("Failed to fetch products", error);
+    return []
+  }
+
+}
+function listProducts(products:Product[]):void{
+  const productContainer = document.getElementById("product-container");
+  if(!productContainer){
+    console.error("Product container not found")
+    return
+  }
+  productContainer.innerHTML= products.map(product=>createCard(product) ).join("")
+
+
+}
+function formatPrice(price:Number){
+  return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+function formatInstallment(installment:Number[]){
+  return `até ${installment[0]}x de ${formatPrice(installment[1])}`
+}
+
+function createCard(product:Product):string{
+  return `<div class="product-card">
+            <img src="${product.image}" alt="">
+            <div class="product-card__description">
+              <h3>${product.name}</h3>
+              <p  class="price">${formatPrice(product.price)}<p>
+              <p>${formatInstallment(product.parcelamento)}</p>
+              <button>Comprar</button>
+            </div>
+          </div>`
 }
 
 document.addEventListener("DOMContentLoaded", main);
